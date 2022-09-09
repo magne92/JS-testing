76e302030f7e15f593aa6238c4414a7829de597f
magne92 <92579836+magne92@users.noreply.github.com>
2022-09-09 09:57:31 +0200

    Update script.js
    added if else checks for correct, low or high guesses

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ var knapp_EL = document.querySelector("#knappID");
 var input_EL = document.querySelector("#inputID");
 var feedback_EL = document.querySelector("#feedback");
 
-
+var random_num = Math.floor(Math.random() * 100) + 1;
 
 function sjekkTall(){
-    console.log(input_EL.value)
+    console.log(input_EL.value);
+    guessed_num = input_EL.value;
+
+    if (guessed_num == random_num){
+        feedback_EL.innerHTML = "Congratulations you win"
+    } else if (guessed_num < random_num ){
+        feedback_EL.innerHTML = "The number you guessed was too low"
+    } else if ( guessed_num > random_num) {
+        feedback_EL.innerHTML = "The number you guessed was too high"
+    }
 }
 
 knapp_EL.addEventListener("click", sjekkTall);
