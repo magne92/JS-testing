63dae5c9fb11c655460eaa29851d1bb74a3feceb
magne92 <92579836+magne92@users.noreply.github.com>
2022-09-12 08:30:51 +0200

    added restart button, attempt and score

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 var knapp_EL = document.querySelector("#knappID");
 var input_EL = document.querySelector("#inputID");
 var feedback_EL = document.querySelector("#feedback");
+var restart_EL = document.querySelector("restart")
 
 var random_num = Math.floor(Math.random() * 100) + 1;
+var attempts = 5;
+var score = 0;
+var playing = true;
 
-function sjekkTall(){
-    console.log(input_EL.value);
-    guessed_num = input_EL.value;
 
-    if (guessed_num == random_num){
-        feedback_EL.innerHTML = "Congratulations you win"
-    } else if (guessed_num < random_num ){
-        feedback_EL.innerHTML = "The number you guessed was too low"
-    } else if ( guessed_num > random_num) {
-        feedback_EL.innerHTML = "The number you guessed was too high"
+function restart(){
+    attempts = 5;
+    score = 0;
+    playing = true
+}
+
+function check_attempts(){
+    if (attempts <= 0) {
+        feedback_EL.innerHTML = "You have no more attempts left. Restart.";
+        playing = false
+
     }
 }
 
+function sjekkTall(){
+    if (playing == true){
+        console.log(input_EL.value);
+        guessed_num = input_EL.value;
+        attempts--;
+
+        if (guessed_num == random_num){
+            feedback_EL.innerHTML = "Congratulations you win";
+            score++;
+            playing = false;
+
+        } else if (guessed_num < random_num ){
+            feedback_EL.innerHTML = "The number you guessed was too low";
+            check_attempts()
+            
+        } else if ( guessed_num > random_num) {
+            feedback_EL.innerHTML = "The number you guessed was too high";
+            check_attempts()
+        }
+
+    } // end if playing
+} // end sjekktall function
 knapp_EL.addEventListener("click", sjekkTall);
 
